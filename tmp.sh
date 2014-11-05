eb51ee03cb41897c44b754bc8ab7d21ed4e47031
dwkns <dwkns@me.com>
2014-11-05 19:29:52 +0000

    Initial commit

---
+++ tmp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+echo ""
+echo "Setting your computer name (as done via System Preferences & Sharing)"
+echo "What would you like it to be?"
+read COMPUTER_NAME
+sudo scutil --set ComputerName $COMPUTER_NAME
+sudo scutil --set HostName $COMPUTER_NAME
+sudo scutil --set LocalHostName $COMPUTER_NAME
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+
