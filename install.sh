5ef3a0d85c404c05fc2e6b376f3ac298ac912e84
dwkns <dwkns@me.com>
2015-01-30 17:42:48 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -268,7 +268,13 @@ if test ! $(which brew); then
   echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
+ if $clean
+ then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+ else
   echo "Homebrew is already installed"
+  fi
 fi
 
 echo ""
