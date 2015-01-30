0ef960c30cb7cec09d8683392fc49ef6c9b29380
dwkns <dwkns@me.com>
2015-01-30 17:23:29 +0000

    Re-odered things

--- config-files/clean.sh
+++ config-files/clean.sh
@@ -8,9 +8,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo rm  -rf /Library/Caches/Homebrew
-sudo rm  -rf /usr/local/*
-sudo rm  -rf /opt/homebrew-cask
+
 
 which -s brew
 
@@ -18,9 +16,12 @@ if [[ $? = 0 ]] ; then
     echo "--- Brew installed"
     brew cleanup
 else
-    echo "--- Brew not installed - not a problem, just letting you know"
+    echo "--- Brew not detected - not a problem, just letting you know"
 fi
 
+sudo rm  -rf /Library/Caches/Homebrew
+sudo rm  -rf /usr/local/*
+sudo rm  -rf /opt/homebrew-cask
 sudo rm -rf .bash_profile .git* .rspec .profile .zshrc .zlogin .bashrc
 sudo rm -rf ~/.rvm  ~/.gem ~/.dropbox ~/.subversion
 sudo rm -rf .bash_profile .git* .rspec
@@ -39,5 +40,5 @@ cd ~/Desktop
 
 
 echo
-echo "--- Got to the end - all done - probably best to open a new terminal before you do anything."
+echo "--- Got to the end - all done"
 echo
