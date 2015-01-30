9832476fe185bdfded96cc3646cf8db5c4e637e6
dwkns <dwkns@me.com>
2015-01-30 22:24:24 +0000

    Update clean.sh

--- config-files/clean.sh
+++ config-files/clean.sh
@@ -18,6 +18,7 @@ if [[ $? = 0 ]] ; then
 else
     echo "--- Brew not detected - not a problem, just letting you know"
 fi
+sudo rm -rf /usr/bin/motion /Library/RubyMotion
 sudo rm -rf /usr/local/.git
 sudo rm  -rf /Library/Caches/Homebrew
 sudo rm  -rf /usr/local/*
