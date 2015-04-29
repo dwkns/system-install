93a371537d8eedb66d5d0361edac00c41b92ec50
dwkns <dwkns@me.com>
2015-04-29 10:56:28 +0100

    Bug fixes with the postgres install

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ PR="\n\033[0;31m==============>\033[1;37m"
 
 MACHINE_NAME="dwkns-mbp"
 BASE_URL="https://raw.githubusercontent.com/dwkns/system-install/master"
+LOCAL_SCRIPTS=true
+SOFTWARE_UPDATE=false
 
  ################### FUNCTIONS ###################
  download_and_run() {
@@ -22,33 +24,50 @@ sudo -v # ask for sudo upfront
 echo -e "$PG Starting Install"
 
 TEMP_DIR=`mktemp -d -t osx-install` # Create a tmp directoy
-echo $TEMP_DIR
 
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-download_and_run scripts/clean.sh
-download_and_run scripts/homebrew.sh
-#download_and_run scripts/postgres.sh
-download_and_run scripts/casks.sh
-download_and_run scripts/node.sh
-download_and_run scripts/krep.sh
-download_and_run scripts/dotfiles.sh
-download_and_run scripts/system-settings.sh
-
-download_and_run scripts/sublime.sh
-download_and_run scripts/rvm-ruby.sh
-download_and_run scripts/gems.sh
+if $LOCAL_SCRIPTS; then
+    echo -e "$PG using local scripts"
+    source scripts/clean.sh
+    source scripts/homebrew.sh
+    source scripts/postgres.sh
+    source scripts/casks.sh
+    source scripts/node.sh
+    source scripts/krep.sh
+    source scripts/dotfiles.sh
+    source scripts/system-settings.sh
+    source scripts/sublime.sh
+    source scripts/rvm-ruby.sh
+    source scripts/gems.sh
+else
+    echo -e "$PG using remote scripts"
+    download_and_run $BASE_URL/scripts/clean.sh
+    download_and_run $BASE_URL/scripts/homebrew.sh
+    download_and_run $BASE_URL/scripts/postgres.sh
+    download_and_run $BASE_URL/scripts/casks.sh
+    download_and_run $BASE_URL/scripts/node.sh
+    download_and_run $BASE_URL/scripts/krep.sh
+    download_and_run $BASE_URL/scripts/dotfiles.sh
+    download_and_run $BASE_URL/scripts/system-settings.sh
+    download_and_run $BASE_URL/scripts/sublime.sh
+    download_and_run $BASE_URL/scripts/rvm-ruby.sh
+    download_and_run $BASE_URL/scripts/gems.sh
+fi
 
 ## Install any Apple System Updates
-# sudo softwareupdate -ia
+if $SOFTWARE_UPDATE; then
+    sudo softwareupdate -ia
+fi
+
 ######################## cleanup ########################
 
 echo -e "$PG  load bash profile into shell"
 source $HOME/.bash_profile
 
 
-killall Dock
+#killall Dock
 echo -e "$PG Deleteing Temp Directory"
 rm -rf $TEMP_DIR
 echo -e "$PG All done \033[0;32m<=======\033[1;37m\n"
