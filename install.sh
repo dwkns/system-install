63395bb2ed7d880b38a647bcee85c1db4fef31b5
dwkns <dwkns@me.com>
2015-04-29 08:52:39 +0100

    Added hellp.sh for testing

--- install.sh
+++ install.sh
@@ -13,7 +13,8 @@ download_and_run() {
   URL="$1"
   FILENAME=`echo $URL | sed 's/.*\///'` # get just the filename from the URL
   curl -s -L $URL -o $TEMP_DIR/$FILENAME # download the file
-  source $TEMP_DIR/$FILENAME # run it
+ #  chmod +x $TEMP_DIR/$FILENAME
+ # source $TEMP_DIR/$FILENAME # run it
 }
 
 ######################## SCRIPT ########################
@@ -21,31 +22,31 @@ sudo -v # ask for sudo upfront
 echo -e "$PG Starting Install"
 
 TEMP_DIR=`mktemp -d -t osx-install` # Create a tmp directoy
-
+echo $TEMP_DIR
 # Keep-alive: update existing sudo time stamp if set, otherwise do nothing.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
 # # download_and_run URL scriptname
-# download_and_run $BASE_URL/sripts/hello.sh
-download_and_run $BASE_URL/sripts/clean.sh
-download_and_run $BASE_URL/sripts/homebrew.sh
-download_and_run $BASE_URL/sripts/postgres.sh
-download_and_run $BASE_URL/sripts/casks.sh
-download_and_run $BASE_URL/sripts/node.sh
-download_and_run $BASE_URL/sripts/krep.sh
-download_and_run $BASE_URL/sripts/dotfiles.sh
-download_and_run $BASE_URL/sripts/system-settings.sh
+download_and_run $BASE_URL/sripts/hello.sh
+# download_and_run $BASE_URL/sripts/clean.sh
+# download_and_run $BASE_URL/sripts/homebrew.sh
+# download_and_run $BASE_URL/sripts/postgres.sh
+# download_and_run $BASE_URL/sripts/casks.sh
+# download_and_run $BASE_URL/sripts/node.sh
+# download_and_run $BASE_URL/sripts/krep.sh
+# download_and_run $BASE_URL/sripts/dotfiles.sh
+# download_and_run $BASE_URL/sripts/system-settings.sh
 
-download_and_run $BASE_URL/sripts/sublime.sh
-download_and_run $BASE_URL/sripts/rvm-ruby.sh
-download_and_run $BASE_URL/sripts/gems.sh
+# download_and_run $BASE_URL/sripts/sublime.sh
+# download_and_run $BASE_URL/sripts/rvm-ruby.sh
+# download_and_run $BASE_URL/sripts/gems.sh
 
 
 ######################## cleanup ########################
 
 echo -e "$PG  load bash profile into shell"
-source $HOME/.bash_profile
+# source $HOME/.bash_profile
 
 killall Dock
 echo -e "$PG Deleteing Temp Directory"
