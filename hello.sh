a592150f5060767cf6d95590e4c763aacc6695c7
dwkns <dwkns@me.com>
2015-05-01 20:47:28 +0100

    updated profiles and settings

--- hello.sh
+++ hello.sh
@@ -1,35 +1,6 @@
  #!/bin/bash
- # Just a little testing ground for things.
-
-
-#  echo "hello"
-
-# if xcode-select -p; then
-#  echo "tools installed";
-# else
-#  echo "tools not installed";
-# fi
-
-
-# osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Library/Desktop Pictures/Solid Colors/Solid Aqua Dark Blue.png"'
-
-
-#reset iTerm 2
-rm -rfv "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
-rm -rfv "$HOME/Library/Application Support/iTerm"
-rm -rfv "$HOME/Library/Application Support/iTerm2"
-rm -rfv "$HOME/Library/Caches/com.googlecode.iterm2"
-killall cfprefsd
-open /Applications/iTerm.app
-
-
-
-sleep 5 # Quit iTerm while sleeping.
-
-
-
-defaults delete com.googlecode.iterm2
-URL="https://raw.githubusercontent.com/dwkns/system-install/master/system-config-files/com.googlecode.iterm2.plist"
-curl $URL > "$HOME/Library/Preferences/com.googlecode.iterm2.plist"
-defaults read -app iTerm
-open /Applications/iTerm.app
+ if command -v dockutil > /dev/null 2>&1; then
+  echo "It's there"
+  else
+  echo "it's not"
+fi
