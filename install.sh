ff96c72332e371b34fef26528f2b8a7dc2515f04
dwkns <dwkns@me.com>
2015-10-11 13:36:59 +0100

    updated

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-
+# set up some colours
+note () {
+  echo -e "\n\033[0;34m====> $1 \033[0m"
+}
+msg () {
+  echo -e "\n\033[0;32m==============> $1 \033[0m"
+}
+warn () {
+  echo -e "\n\033[0;31m====> $1 \033[0m"
+}
 
 # ask for sudo upfront
 sudo -v 
