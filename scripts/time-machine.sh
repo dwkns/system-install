08d8b49f294f0052bb05883d44aa0bc5b39cab20
dwkns <dwkns@me.com>
2015-10-12 16:45:09 +0100

    updated theme

--- scripts/time-machine.sh
+++ scripts/time-machine.sh
@@ -1,4 +1,6 @@
 #!/bin/bash 
+source "$HOME/.system-config/scripts/colours.sh"
+
 ######################## Time Machine ########################
 msg "Adding Time Machine Exclusions"
 
@@ -9,6 +11,16 @@ TIME_MACHINE_EXCLUSION_LIST=(
   "$HOME/Documents/Parallels/"
   "$HOME/Dropbox/"
   "$HOME/Library/Application Support/Google/"
+  "$HOME/Library/Calendars/Calendar Cache"
+  "$HOME/Music/iTunes/iTunes Music Library.xml"
+  "$HOME/Library/iTunes/iPod Software Updates"
+  "$HOME/Library/iTunes/iPad Software Updates"
+  "$HOME/Library/iTunes/iPhone Software Updates"
+  "$HOME/Pictures/iPod Photo Cache"
+  "$HOME/Music/iTunes/Album Artwork/Cache"
+  "$HOME/Library/Saved Application State"
+  "$HOME/Library/Icons/WebpageIcons.db"
+  "$HOME/Library/Safari/WebpageIcons.db"
 )
 
 for LOCATION in "${TIME_MACHINE_EXCLUSION_LIST[@]}"
@@ -30,7 +42,7 @@ defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
 
 warn "~/Dropbox has been added to the time machine exclusion list"
 echo "If you want to remove the exclusion run :"
-echo "tmutil removeexclusion '~/Dropbox/'"
+echo "sudo tmutil removeexclusion '$HOME/Dropbox/'"
 
 note "done"
 
