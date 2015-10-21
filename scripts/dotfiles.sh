7399a1515d9e6075d321a843a3e82ee85a67a90c
dwkns <dwkns@me.com>
2015-10-21 12:12:46 +0100

    updated theme

---
+++ scripts/dotfiles.sh
@@ -0,0 +1,22 @@
+source "$HOME/.system-config/scripts/colours.sh"
+##################### Configure Sublime ######################
+
+msg "Copying dotfiles"
+
+ROOT_DIR="$HOME/.system-config"
+
+echo "Copying .bash_profile"
+cp -f "$ROOT_DIR/system-config-files/bash.bash_profile"  "$HOME/.bash_profile"
+
+echo "Copying .rspec"
+cp -f  "$ROOT_DIR/system-config-files/bash.rspec" "$HOME/.rspec"
+
+echo "Copying .gemrc"
+cp -f "$ROOT_DIR/system-config-files/bash.gemrc" "$HOME/.gemrc"
+
+echo "Copying .gitconfig"
+cp -f "$ROOT_DIR/system-config-files/bash.gitconfig" "$HOME/.gitconfig"
+
+echo "Copying .gitignore"
+cp -f "$ROOT_DIR/system-config-files/bash.gitignore_global" "$HOME/.gitignore_global"
+note "done"
