17f7060fb87ee132443dc2a35aacc4b1e1bb39bf
dwkns <dwkns@me.com>
2015-10-21 12:12:46 +0100

    updated theme

--- install.sh
+++ install.sh
@@ -35,24 +35,12 @@ else
 fi
 
 
+
 ######################## DOTFILES ########################
-msg "Copying dotfiles"
+source "$ROOT_DIR/scripts/dotfiles.sh"
 
-echo "Copying .bash_profile"
-cp -f "$ROOT_DIR/system-config-files/bash.bash_profile"  "$HOME/.bash_profile"
 
-echo "Copying .rspec"
-cp -f  "$ROOT_DIR/system-config-files/bash.rspec" "$HOME/.rspec"
 
-echo "Copying .gemrc"
-cp -f "$ROOT_DIR/system-config-files/bash.gemrc" "$HOME/.gemrc"
-
-echo "Copying .gitconfig"
-cp -f "$ROOT_DIR/system-config-files/bash.gitconfig" "$HOME/.gitconfig"
-
-echo "Copying .gitignore"
-cp -f "$ROOT_DIR/system-config-files/bash.gitignore_global" "$HOME/.gitignore_global"
-note "done"
 
 ###################### configure git ######################
 msg "Configuring git"
