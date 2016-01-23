f2da1bbcee87e04bde8faadc8a5d6fd186dd1437
dwkns <dwkns@me.com>
2016-01-23 13:30:35 +0000

    Updated the filenames

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -6,20 +6,20 @@ msg "Copying dotfiles"
 ROOT_DIR="$HOME/.system-config"
 
 echo "Copying .bash_profile"
-cp -f "$ROOT_DIR/system-config-files/bash.bash_profile"  "$HOME/.bash_profile"
+cp -f "$ROOT_DIR/system-config-files/.bash_profile"  "$HOME/.bash_profile"
 
 echo "Copying .jsbeautifyrc"
-cp -f "$ROOT_DIR/system-config-files/bash.jsbeautifyrc"  "$HOME/.jsbeautifyrc"
+cp -f "$ROOT_DIR/system-config-files/.jsbeautifyrc"  "$HOME/.jsbeautifyrc"
 
 echo "Copying .rspec"
-cp -f  "$ROOT_DIR/system-config-files/bash.rspec" "$HOME/.rspec"
+cp -f  "$ROOT_DIR/system-config-files/.rspec" "$HOME/.rspec"
 
 echo "Copying .gemrc"
-cp -f "$ROOT_DIR/system-config-files/bash.gemrc" "$HOME/.gemrc"
+cp -f "$ROOT_DIR/system-config-files/.gemrc" "$HOME/.gemrc"
 
 echo "Copying .gitconfig"
-cp -f "$ROOT_DIR/system-config-files/bash.gitconfig" "$HOME/.gitconfig"
+cp -f "$ROOT_DIR/system-config-files/.gitconfig" "$HOME/.gitconfig"
 
 echo "Copying .gitignore"
-cp -f "$ROOT_DIR/system-config-files/bash.gitignore_global" "$HOME/.gitignore_global"
+cp -f "$ROOT_DIR/system-config-files/.gitignore_global" "$HOME/.gitignore_global"
 note "done"
