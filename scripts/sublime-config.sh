c438c05a2e88479861dbf7bff6f51ca1ab862160
dwkns <dwkns@me.com>
2018-10-09 10:03:43 +0100

    Updated Config Files

--- scripts/sublime-config.sh
+++ scripts/sublime-config.sh
@@ -1,59 +1,57 @@
- 
-   source "$HOME/.system-config/scripts/colours.sh"
+  source "$HOME/.system-config/scripts/colours.sh"
+
   ##################### Configure Sublime ######################
+  #!/usr/bin/env bash
   msg "Configure sublime"
 
   
+  # Set up some defaults
   ROOT_DIR="$HOME/.system-config"
   SUBLIME="$HOME/Library/Application Support/Sublime Text 3"
-  SUBLIME_LOCAL_DIR="$SUBLIME/local"
-  SUBLIME_PACKAGES_DIR="$SUBLIME/Packages"
-  SUBLIME_USER_DIR="$SUBLIME/Packages/User"
-  SUBLIME_INSTALLED_PACKAGES_DIR="$SUBLIME/Installed Packages"
+  SUBLIME_LOCAL="$SUBLIME/local"
+  SUBLIME_PACKAGES="$SUBLIME/Packages"
+  SUBLIME_USER="$SUBLIME/Packages/User"
+  SUBLIME_INSTALLED_PACKAGES="$SUBLIME/Installed Packages"
+  DWKNS_SETTINGS="$SUBLIME_USER/dwkns-sublime-settings"
 
 
+  msg "Removing the existing Sublime User folder"
+  rm -rf "$SUBLIME_USER" # clean the Sublime user folder
 
-  mkdir -p "$SUBLIME_LOCAL_DIR"
-  mkdir -p "$SUBLIME_USER_DIR"
-  mkdir -p "$SUBLIME_INSTALLED_PACKAGES_DIR"
+  mkdir -p "$SUBLIME_LOCAL"
+  mkdir -p "$SUBLIME_USER"
+  mkdir -p "$SUBLIME_INSTALLED_PACKAGES"
 
+  ### first clone in the settings folder
+  msg "Cloning in dwkns settings"
+  rm -rf "$SUBLIME_USER/dwkns-sublime-settings"
+  git clone "https://github.com/dwkns/dwkns-sublime-settings.git" "$SUBLIME_USER/dwkns-sublime-settings"
+  
   msg "Intalling license"
-  cp -f "$ROOT_DIR/sublime-config-files/License.sublime_license" "$SUBLIME_LOCAL_DIR/License.sublime_license"
+  cp -f "$DWKNS_SETTINGS/License.sublime_license" "$SUBLIME_LOCAL/License.sublime_license"
 
   msg "Installing Package Control"
-  curl -L "http://sublime.wbond.net/Package Control.sublime-package" -o "$SUBLIME_INSTALLED_PACKAGES_DIR/Package Control.sublime-package"
+  curl -L "http://sublime.wbond.net/Package Control.sublime-package" -o "$SUBLIME_INSTALLED_PACKAGES/Package Control.sublime-package"
 
- msg "Installing sublime Package Control Settings"
-  cp -f "$ROOT_DIR/sublime-config-files/Package Control.sublime-settings" "$SUBLIME_USER_DIR/Package Control.sublime-settings"
+  msg "Creating Symbolic links for various files"
+  DWKNS_SETTINGS="dwkns-sublime-settings"
+  files=( 
+    "Preferences.sublime-settings"
+    "Package Control.sublime-settings"
+    "SublimeLinter.sublime-settings"
+    "scope_hunter.sublime-settings"
+    "BeautifyRuby.sublime-settings"
+    "Default (OSX).sublime-keymap"
+  )
 
-  msg "Installing sublime preferences"
-  cp -f "$ROOT_DIR/sublime-config-files/Preferences.sublime-settings" "$SUBLIME_USER_DIR/Preferences.sublime-settings"
-
- msg "Installing Douglas Colour Theme"
-  cp -f "$ROOT_DIR/sublime-config-files/Douglas.sublime-color-scheme" "$SUBLIME_USER_DIR/Douglas.sublime-color-scheme"
-
-
-  msg "Installing SublimeLinter preferences"
-  cp -f "$ROOT_DIR/sublime-config-files/SublimeLinter.sublime-settings" "$SUBLIME_USER_DIR/SublimeLinter.sublime-settings"
-
-  msg "Installing Emmet preferences"
-  cp -f "$ROOT_DIR/sublime-config-files/Emmet.sublime-settings" "$SUBLIME_USER_DIR/Emmet.sublime-settings"
-
-  msg "Installing phpfmt preferences"
-  cp -f "$ROOT_DIR/sublime-config-files/phpfmt.sublime-settings" "$SUBLIME_USER_DIR/phpfmt.sublime-settings"
-  
-  msg "Cloning in my Macros"
-  rm -rf "$SUBLIME_USER_DIR/custom_macros"
-  git clone "https://github.com/dwkns/sublime_macros.git" "$SUBLIME_USER_DIR/custom_macros"
- 
+  for i in "${files[@]}"
+  do
+     rm -f "$DWKNS_SETTINGS/$i"
+     ln -s "$DWKNS_SETTINGS/settings/$i" "$SUBLIME_USER/$i"
+  done
 
   msg "Cloning in my Rails snippets"
-  rm -rf "$SUBLIME_PACKAGES_DIR/Rails"
-  git clone "https://github.com/dwkns/sublime_rails_snippets.git" "$SUBLIME_PACKAGES_DIR/Rails"
-
-
-  msg "Installing sublime keymap"
-  cp -f "$ROOT_DIR/sublime-config-files/Default (OSX).sublime-keymap" "$SUBLIME_USER_DIR/Default (OSX).sublime-keymap"
-
+  rm -rf "$SUBLIME_PACKAGES/Rails"
+  git clone "https://github.com/dwkns/sublime_rails_snippets.git" "$SUBLIME_PACKAGES/Rails"
 
   note "done"
