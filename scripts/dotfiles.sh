b9e51321e82c7f528e79a652d80c30201593af1e
dwkns <dwkns@me.com>
2018-10-10 19:15:02 +0100

    Updated Config Files

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -3,31 +3,53 @@ source "$HOME/.system-config/scripts/colours.sh"
 
 msg "Copying dotfiles"
 
+# echo "Copying .bash_profile"
+# cp -f "$ROOT_DIR/system-config-files/.bash_profile"  "$HOME/.bash_profile"
+
+# echo "Copying .gemrc"
+# cp -f "$ROOT_DIR/system-config-files/.gemrc" "$HOME/.gemrc"
+
+# echo "Copying .gitconfig"
+# cp -f "$ROOT_DIR/system-config-files/.gitconfig" "$HOME/.gitconfig"
+
+# echo "Copying .gitignore"
+# cp -f "$ROOT_DIR/system-config-files/.gitignore_global" "$HOME/.gitignore_global"
+
+# echo "Copying .irbrc"
+# cp -f  "$ROOT_DIR/system-config-files/.irbrc" "$HOME/.irbrc"
+
+# # echo "Copying .jsbeautifyrc"
+# # cp -f "$ROOT_DIR/system-config-files/.jsbeautifyrc"  "$HOME/.jsbeautifyrc"
+
+# echo "Copying .rspec"
+# cp -f  "$ROOT_DIR/system-config-files/.rspec" "$HOME/.rspec"
+
+# echo "Copying .profile"
+# cp -f  "$ROOT_DIR/system-config-files/.profile" "$HOME/.profile"
+
+
 ROOT_DIR="$HOME/.system-config"
 
-echo "Copying .bash_profile"
-cp -f "$ROOT_DIR/system-config-files/.bash_profile"  "$HOME/.bash_profile"
+DOTFILES=( 
+  ".bash_profile"
+  ".gemrc"
+  ".gitconfig"
+  ".gitignore_global"
+  ".irbrc"
+  ".rspec"
+  ".jsbeautifyrc"
+  ".eslintrc.yaml"
+)  
 
-echo "Copying .gemrc"
-cp -f "$ROOT_DIR/system-config-files/.gemrc" "$HOME/.gemrc"
+installDotFiles () {
+  smsg 'Copying dotfiles...';
+  for THISFILE in "${DOTFILES[@]}"
+  do  
+    echo "Installing : $THISFILE"
+    cp -f "$ROOT_DIR/system-config-files/$THISFILE"  "$HOME/$THISFILE"
+  done
+}
 
-echo "Copying .gitconfig"
-cp -f "$ROOT_DIR/system-config-files/.gitconfig" "$HOME/.gitconfig"
-
-echo "Copying .gitignore"
-cp -f "$ROOT_DIR/system-config-files/.gitignore_global" "$HOME/.gitignore_global"
-
-echo "Copying .irbrc"
-cp -f  "$ROOT_DIR/system-config-files/.irbrc" "$HOME/.irbrc"
-
-# echo "Copying .jsbeautifyrc"
-# cp -f "$ROOT_DIR/system-config-files/.jsbeautifyrc"  "$HOME/.jsbeautifyrc"
-
-echo "Copying .rspec"
-cp -f  "$ROOT_DIR/system-config-files/.rspec" "$HOME/.rspec"
-
-echo "Copying .profile"
-cp -f  "$ROOT_DIR/system-config-files/.profile" "$HOME/.profile"
-
+installDotFiles
 
 note "done"
