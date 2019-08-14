6fb254b425bec2bfcf55ef6f7593fb316cba6a17
dwkns <dwkns@me.com>
2019-08-14 21:20:45 +0100

    Updated Config Files

--- scripts/system-settings.sh
+++ scripts/system-settings.sh
@@ -37,7 +37,7 @@ echo "General : Set a blazingly fast keyboard repeat rate"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1 # normal minimum is 2 (30 ms)
+defaults write NSGlobalDomain KeyRepeat -int 2 # normal minimum is 2 (30 ms)
 defaults write NSGlobalDomain InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
 
 echo                        
