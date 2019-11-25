3df72070d5aacf9c513a04c1cb0d605c5f1ff7d1
dwkns <dwkns@me.com>
2019-11-25 14:57:48 +0000

    Updated colour

--- scripts/utils/colours.sh
+++ scripts/utils/colours.sh
@@ -5,6 +5,9 @@ YELLOW="\033[0;33m"
 GREEN="\033[0;32m"          
 BLUE="\033[0;94m"
 RESET="\033[0m"
+CYAN="\033[0;36m"
+
+
 
 ############################### Functions ###############################
 
@@ -22,4 +25,8 @@ error () {
 
 note () {
   echo -e $RESET"====>$RESET $1"
+}
+
+msg () {
+  echo -e $CYAN"Warning ====>$RESET $1"
 }
