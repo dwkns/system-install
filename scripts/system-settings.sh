f6635394120b57cf0ec754629dd4dd8546a629eb
dwkns <dwkns@me.com>
2019-11-25 15:06:12 +0000

    Updated Config Files

--- scripts/system-settings.sh
+++ scripts/system-settings.sh
@@ -17,7 +17,7 @@ defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
 echo $CYAN"General :$RESET Automatically quit printer app once the print jobs complete"
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# echo "Restart automatically if the computer freezes"
+# echo $CYAN"Restart automatically if the computer freezes"
 # sudo systemsetup -setrestartfreeze on
 
 echo $CYAN"General :$RESET Check for software updates daily, not just once per week"
@@ -47,16 +47,16 @@ echo
 ###############################################################################
 
 
-echo "Screen : Save screenshots to the desktop"
+echo $CYAN"Screen :$RESET Save screenshots to the desktop"
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-echo "Screen : Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+echo $CYAN"Screen :$RESET Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
 defaults write com.apple.screencapture type -string "png"
 
-echo "Screen : Disable shadow in screenshots"
+echo $CYAN"Screen :$RESET Disable shadow in screenshots"
 defaults write com.apple.screencapture disable-shadow -bool true
 
-echo "Screen : Enable subpixel font rendering on non-Apple LCDs"
+echo $CYAN"Screen :$RESET Enable subpixel font rendering on non-Apple LCDs"
 defaults write NSGlobalDomain AppleFontSmoothing -int 2
 
 echo
@@ -66,137 +66,137 @@ echo
 # Finder                                                                      #
 ###############################################################################
 
-echo "Finder : Setting interface style to Dark"
+echo $CYAN"Finder :$RESET Setting interface style to Dark"
 defaults write NSGlobalDomain AppleInterfaceStyle Dark
 
-echo "Finder : Expand save panel by default"
+echo $CYAN"Finder :$RESET Expand save panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
-echo "Finder : Expand print panel by default"
+echo $CYAN"Finder :$RESET Expand print panel by default"
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
 
-echo "Finder : Save to disk (not to iCloud) by default"
+echo $CYAN"Finder :$RESET Save to disk (not to iCloud) by default"
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-echo "Finder : Showing all filename extensions in Finder by default"
+echo $CYAN"Finder :$RESET Showing all filename extensions in Finder by default"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-echo "Finder : Showing Library & ~/Library"
+echo $CYAN"Finder :$RESET Showing Library & ~/Library"
 chflags nohidden ~/Library
 chflags nohidden /Library
 
-echo "Finder : Hiding ~/Applications"
+echo $CYAN"Finder :$RESET Hiding ~/Applications"
 chflags hidden ~/Applications
 
-echo "Finder : Set sidebar icon size to small"
+echo $CYAN"Finder :$RESET Set sidebar icon size to small"
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
 
-echo "Finder : Set home folder as the default location for new Finder windows"
+echo $CYAN"Finder :$RESET Set home folder as the default location for new Finder windows"
 # "PfDe" - "file://${HOME}/Desktop/"
 # For other paths, use "PfLo" - "file://${HOME}/Desktop/"
 defaults write com.apple.finder NewWindowTarget -string "PfLo"
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
 
-echo "Finder : Show / hide icons for hard drives, servers, and removable media on the desktop"
+echo $CYAN"Finder :$RESET Show / hide icons for hard drives, servers, and removable media on the desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
-# echo "Finder : show hidden files by default"
+# echo $CYAN"Finder :$RESET show hidden files by default"
 # defaults write com.apple.finder AppleShowAllFiles -bool true
 
-echo "Finder : hide status bar"
+echo $CYAN"Finder :$RESET hide status bar"
 defaults write com.apple.finder ShowStatusBar -bool false
 
-echo "Finder : hide path bar"
+echo $CYAN"Finder :$RESET hide path bar"
 defaults write com.apple.finder ShowPathbar -bool false
 
-echo "Finder : allow text selection in Quick Look"
+echo $CYAN"Finder :$RESET allow text selection in Quick Look"
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
-echo "Finder : Display full POSIX path as Finder window title"
+echo $CYAN"Finder :$RESET Display full POSIX path as Finder window title"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-echo "Finder : When performing a search, search the current folder by default"
+echo $CYAN"Finder :$RESET When performing a search, search the current folder by default"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-echo "Finder : Disable the warning when changing a file extension"
+echo $CYAN"Finder :$RESET Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-echo "Finder : Enable spring loading for directories"
+echo $CYAN"Finder :$RESET Enable spring loading for directories"
 defaults write NSGlobalDomain com.apple.springing.enabled -bool true
 
-echo "Finder : Remove the spring loading delay for directories"
+echo $CYAN"Finder :$RESET Remove the spring loading delay for directories"
 defaults write NSGlobalDomain com.apple.springing.delay -float 0
 
-echo "Finder : Avoid creating .DS_Store files on network volumes"
+echo $CYAN"Finder :$RESET Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-echo "Finder : Disable disk image verification"
+echo $CYAN"Finder :$RESET Disable disk image verification"
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-echo "Finder : Automatically open a new Finder window when a volume is mounted"
+echo $CYAN"Finder :$RESET Automatically open a new Finder window when a volume is mounted"
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 
-# echo "Finder : Increase grid spacing for icons on the desktop and in other icon views"
+# echo $CYAN"Finder :$RESET Increase grid spacing for icons on the desktop and in other icon views"
 # /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 85" ~/Library/Preferences/com.apple.finder.plist
 # /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:gridSpacing 85" ~/Library/Preferences/com.apple.finder.plist
 # /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:gridSpacing 85" ~/Library/Preferences/com.apple.finder.plist
 
-# echo "Finder : Increase the size of icons on the desktop and in other icon views"
+# echo $CYAN"Finder :$RESET Increase the size of icons on the desktop and in other icon views"
 # /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 50" ~/Library/Preferences/com.apple.finder.plist
 # /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:iconSize 50" ~/Library/Preferences/com.apple.finder.plist
 # /usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:iconSize 50" ~/Library/Preferences/com.apple.finder.plist
 
-echo "Finder : Use column view in all Finder windows by default"
+echo $CYAN"Finder :$RESET Use column view in all Finder windows by default"
 # Flwv ▸ Cover Flow View
 # Nlsv ▸ List View
 # clmv ▸ Column View
 # icnv ▸ Icon View
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
-echo "Finder : Disable the warning before emptying the Trash"
+echo $CYAN"Finder :$RESET Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false
 
-echo "Finder : Set the icon size of Dock items to 36 pixels"
+echo $CYAN"Finder :$RESET Set the icon size of Dock items to 36 pixels"
 defaults write com.apple.dock tilesize -int 36
 
-echo "Finder : Show indicator lights for open applications in the Dock"
+echo $CYAN"Finder :$RESET Show indicator lights for open applications in the Dock"
 defaults write com.apple.dock show-process-indicators -bool true
 
-echo "Finder : Speed up Mission Control animations"
+echo $CYAN"Finder :$RESET Speed up Mission Control animations"
 defaults write com.apple.dock expose-animation-duration -float 0.1
 
 # # Don’t group windows by application in Mission Control
 # # (i.e. use the old Exposé behavior instead)
 # defaults write com.apple.dock expose-group-by-app -bool false
 
-echo "Finder : Disable Dashboard"
+echo $CYAN"Finder :$RESET Disable Dashboard"
 defaults write com.apple.dashboard mcx-disabled -bool true
 
-echo "Finder : Don’t show Dashboard as a Space"
+echo $CYAN"Finder :$RESET Don’t show Dashboard as a Space"
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
-echo "Finder : Don’t automatically rearrange Spaces based on most recent use"
+echo $CYAN"Finder :$RESET Don’t automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
 
-echo "Finder : Remove the auto-hiding Dock delay"
+echo $CYAN"Finder :$RESET Remove the auto-hiding Dock delay"
 defaults write com.apple.dock autohide-delay -float 0
 
-echo "Finder : Remove the animation when hiding/showing the Dock"
+echo $CYAN"Finder :$RESET Remove the animation when hiding/showing the Dock"
 defaults write com.apple.dock autohide-time-modifier -float 0
 
-echo "Finder : Automatically hide and show the Dock"
+echo $CYAN"Finder :$RESET Automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool false
 
-# echo "Finder : Make Dock icons of hidden applications translucent"
+# echo $CYAN"Finder :$RESET Make Dock icons of hidden applications translucent"
 # defaults write com.apple.dock showhidden -bool false
 
 echo
@@ -205,36 +205,36 @@ echo
 # Safari & WebKit                                                             #
 ###############################################################################
 
-echo "Safari : UniversalSearchEnabled set to true"
+echo $CYAN"Safari :$RESET UniversalSearchEnabled set to true"
 defaults write com.apple.Safari UniversalSearchEnabled -bool true
 
-echo "Safari : SuppressSearchSuggestions set to false"
+echo $CYAN"Safari :$RESET SuppressSearchSuggestions set to false"
 defaults write com.apple.Safari SuppressSearchSuggestions -bool false
 
-echo "Safari : Show the full URL in the address bar (note: this still hides the scheme)"
+echo $CYAN"Safari :$RESET Show the full URL in the address bar (note: this still hides the scheme)"
 defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
-echo "Safari : Set Safari’s home page to about:blank for faster loading"
+echo $CYAN"Safari :$RESET Set Safari’s home page to about:blank for faster loading"
 defaults write com.apple.Safari HomePage -string "about:blank"
 
-echo "Safari : Show Safari’s bookmarks bar by default"
+echo $CYAN"Safari :$RESET Show Safari’s bookmarks bar by default"
 defaults write com.apple.Safari ShowFavoritesBar -bool true
 
-echo "Safari : Hide Safari’s sidebar in Top Sites"
+echo $CYAN"Safari :$RESET Hide Safari’s sidebar in Top Sites"
 defaults write com.apple.Safari ShowSidebarInTopSites -bool false
 
-echo "Safari : Make Safari’s search banners default to Contains instead of Starts With"
+echo $CYAN"Safari :$RESET Make Safari’s search banners default to Contains instead of Starts With"
 defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
 
-echo "Safari : Remove useless icons from Safari’s bookmarks bar"
+echo $CYAN"Safari :$RESET Remove useless icons from Safari’s bookmarks bar"
 defaults write com.apple.Safari ProxiesInBookmarksBar "()"
 
-echo "Safari : Enable the Develop menu and the Web Inspector in Safari"
+echo $CYAN"Safari :$RESET Enable the Develop menu and the Web Inspector in Safari"
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
-echo "Safari : Add a context menu item for showing the Web Inspector in web views"
+echo $CYAN"Safari :$RESET Add a context menu item for showing the Web Inspector in web views"
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 echo
@@ -243,16 +243,16 @@ echo
 # Activity Monitor                                                            #
 ###############################################################################
 
-echo "Activity Monitor : show the main window when launching"
+echo $CYAN"Activity Monitor :$RESET show the main window when launching"
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-echo "Activity Monitor : Visualize CPU usage in the Dock icon"
+echo $CYAN"Activity Monitor :$RESET Visualize CPU usage in the Dock icon"
 defaults write com.apple.ActivityMonitor IconType -int 5
 
-echo "Activity Monitor : Show all processes"
+echo $CYAN"Activity Monitor :$RESET Show all processes"
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-echo "Activity Monitor : Sort results by CPU usage"
+echo $CYAN"Activity Monitor :$RESET Sort results by CPU usage"
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
@@ -262,18 +262,18 @@ echo
 # Address Book, Dashboard, iCal, TextEdit, and Disk Utility                   #
 ###############################################################################
 
-echo "Address Book : Enable the debug menu"
+echo $CYAN"Address Book :$RESET Enable the debug menu"
 defaults write com.apple.addressbook ABShowDebugMenu -bool true
 
-echo "Address Book : Sort Names by First Name / Last Name"
+echo $CYAN"Address Book :$RESET Sort Names by First Name / Last Name"
 defaults write com.apple.addressbook ABNameSortingFormat -string "sortingFirstName sortingLastName"
 
 echo
 
-echo "TextEdit : Use plain text mode for new = documents"
+echo $CYAN"TextEdit :$RESET Use plain text mode for new = documents"
 defaults write com.apple.TextEdit RichText -int 0
 
-echo "TextEdit : Open and save files as UTF-8 in TextEdit"
+echo $CYAN"TextEdit :$RESET Open and save files as UTF-8 in TextEdit"
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
 
@@ -283,10 +283,10 @@ echo
 # Mac App Store                                                               #
 ###############################################################################
 
-echo "Mac App Store : Enable the WebKit Developer Tools in the Mac App Store"
+echo $CYAN"Mac App Store :$RESET Enable the WebKit Developer Tools in the Mac App Store"
 defaults write com.apple.appstore WebKitDeveloperExtras -bool true
 
-echo "Mac App Store : Enable Debug Menu in the Mac App Store"
+echo $CYAN"Mac App Store :$RESET Enable Debug Menu in the Mac App Store"
 defaults write com.apple.appstore ShowDebugMenu -bool true
 
 echo
@@ -295,27 +295,27 @@ echo
 # Transmission.app                                                            #
 ###############################################################################
 
-echo "Transmission.app : Use ~/Documents/Torrents to store incomplete downloads"
+echo $CYAN"Transmission.app :$RESET Use ~/Documents/Torrents to store incomplete downloads"
 mkdir -p "$HOME/Documents/Torrents"
 defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
 defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Documents/Torrents"
 
-echo "Transmission.app : Don’t prompt for confirmation before downloading"
+echo $CYAN"Transmission.app :$RESET Don’t prompt for confirmation before downloading"
 defaults write org.m0k.transmission DownloadAsk -bool false
 
-echo "Transmission.app : Trash original torrent files"
+echo $CYAN"Transmission.app :$RESET Trash original torrent files"
 defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
 
-echo "Transmission.app : Hide the donate message"
+echo $CYAN"Transmission.app :$RESET Hide the donate message"
 defaults write org.m0k.transmission WarningDonate -bool false
 
-echo "Transmission.app : Hide the legal disclaimer"
+echo $CYAN"Transmission.app :$RESET Hide the legal disclaimer"
 defaults write org.m0k.transmission WarningLegal -bool false
 
 echo
 
 echo
-echo "iTerm2.app : ~/.system-config/system-config-files/ for preferences"
+echo $CYAN"iTerm2.app :$RESET ~/.system-config/system-config-files/ for preferences"
 # Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.system-config/system-config-files/"
 # Tell iTerm2 to use the custom preferences in the directory
@@ -502,7 +502,7 @@ note "done - some settings may require a restart"
   # # Fix for the ancient UTF-8 bug in QuickLook (https://mths.be/bbo)
   # # Commented out, as this is known to cause problems in various Adobe apps :(
   # # See https://github.com/mathiasbynens/dotfiles/issues/237
-  # #echo "0x08000100:0" > ~/.CFUserTextEncoding
+  # #echo $CYAN"0x08000100:0" > ~/.CFUserTextEncoding
 
   # # Reveal IP address, hostname, OS version, etc. when clicking the clock
   # # in the login window
@@ -1324,4 +1324,4 @@ note "done - some settings may require a restart"
   #   "iCal"; do
   #   killall "${app}" &> /dev/null
   # done
-  # echo "Done. Note that some of these changes require a logout/restart to take effect."
+  # echo $CYAN"Done. Note that some of these changes require a logout/restart to take effect."
