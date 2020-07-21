6c7b06bd7ea450b8b73e7967db31059e48103246
dwkns <dwkns@me.com>
2020-07-21 10:49:56 +0100

    Updated Config Files

--- scripts/vscode-config.sh
+++ scripts/vscode-config.sh
@@ -5,9 +5,8 @@
   doing "Configure vscode"
   
   # Set up some defaults
-  mkdir -p "$HOME/.vscode/extensions/dwkns-vs" # make sure it exists
   rm -rf "$HOME/.vscode/extensions/dwkns-vs"
-
+  mkdir -p "$HOME/.vscode/extensions/dwkns-vs" # make sure it exists
   doing "Cloning in dwkns settings"
   git clone https://github.com/dwkns/dwkns-vs.git "$HOME/.vscode/extensions/dwkns-vs"
   note "done"
