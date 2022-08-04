743f2d6b57843cde3957bb97dcf407c1a15aff26
dwkns <dwkns@me.com>
2022-08-04 12:44:17 +0100

    Updated Config Files

--- scripts/colours.sh
+++ scripts/colours.sh
@@ -1,11 +1,25 @@
 #!/usr/bin/env zsh
 ############################### colours ###############################
+WHITE="\033[0;37m"
+GRAY="\033[0;30m"
 RED="\033[0;31m"          
 YELLOW="\033[0;33m"       
 GREEN="\033[0;32m"          
 BLUE="\033[0;94m"
-RESET="\033[0m"
 CYAN="\033[0;36m"
+PURPLE="\033[0;35m"
+WHITE_BOLD="\033[1;37m"
+GRAY_BOLD="\033[1;30m"
+RED_BOLD="\033[1;31m"          
+YELLOW_BOLD="\033[1;33m"       
+GREEN_BOLD="\033[1;32m"          
+BLUE_BOLD="\033[1;94m"
+CYAN_BOLD="\033[1;36m"
+PURPLE_BOLD="\033[1;35m"
+RESET="\033[0m"
+
+
+# $RED colour $YELLOW colour $GREEN colour $BLUE colour $RESET colour $CYAN colour 
 
 
 ############################### emoji ###############################
@@ -32,11 +46,11 @@ error () {
 }
 
 note () {
-  echo -e $YELLOW"Note ========>$RESET $1 $RESET"
+  echo -e $CYAN"Note —>$RESET $1 $RESET"
 }
 
 doing () {
-  echo -e $GREEN"Doing ========>$CYAN $1 $RESET"
+  echo -e $GREEN"Doing ————> $1 $RESET"
 }
 
 complete () {
