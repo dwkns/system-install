07c713bca4b93b5cfa0fc4b4866cfff1940886a0
dwkns <dwkns@me.com>
2022-09-29 14:45:13 +0100

    Updated Config Files

--- scripts/eleventy-tailwind-starter.sh
+++ scripts/eleventy-tailwind-starter.sh
@@ -1,12 +1,10 @@
-
 #!/usr/bin/env zsh
-. $HOME/.system-config/scripts/set-up-projects.sh $1;
+. $HOME/.system-config/scripts/set-up-projects.sh $1
 
 success "Project $PROJECTNAME will be created!"
 
 # git clone https://github.com/dwkns/snowpack-tailwind-11ty-barebones.git $PROJECTNAME
 git clone https://github.com/dwkns/tiny-start.git $PROJECTNAME
-
 
 ######## Create the folder
 cd "$PROJECTNAME"
@@ -14,15 +12,14 @@ cd "$PROJECTNAME"
 ######## Do some processing of package.json to add build scrips & title.
 
 # change the name property of package.json (need to save temp file first)
-tmp=$(mktemp) 
+tmp=$(mktemp)
 JQVAR=".name = \"$PROJECTNAME\""
-jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
-
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
 
 # add author  to package.json
-tmp=$(mktemp) 
+tmp=$(mktemp)
 JQVAR=".author = \"$USER\""
-jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
 
 # # make bin/s executable.
 # mkdir -p bin
@@ -35,9 +32,7 @@ jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
 # create .env file
 cp .env-template .env
 
-
-
-######## Put the project name into the HTML Title. 
+######## Put the project name into the HTML Title.
 sed -i "" -e "s/---project-name---/$PROJECTNAME/g" ./src/index.njk
 
 ######## Initialize git.
@@ -46,12 +41,15 @@ git init
 git add .
 git commit -m "Initial commit"
 
-  # rm -rf node_modules.nosync
-  # rm -rf yarn.lock
-  # doing 'removing existing node_modules folder'; rm -rf node_modules
-  # doing 'removing existing node_modules folder'; rm -rf 'node_modules 2'
-  # doing 'creating node_modules.nosync'; mkdir node_modules.nosync
-  # doing 'creating symlink '; ln -s node_modules.nosync/ node_modules
-  doing 'running yarn'; yarn install
+# rm -rf node_modules.nosync
+# rm -rf yarn.lock
+# doing 'removing existing node_modules folder'; rm -rf node_modules
+# doing 'removing existing node_modules folder'; rm -rf 'node_modules 2'
+# doing 'creating node_modules.nosync'; mkdir node_modules.nosync
+# doing 'creating symlink '; ln -s node_modules.nosync/ node_modules
+
+doing 'running yarn'
+yarn install
+npx install-peerdeps --dev eslint-config-wesbos && echo '{"extends": ["wesbos"]}' >>.eslintrc
 
 code -r .
