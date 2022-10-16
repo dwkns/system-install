4eab4918ee6e15c356b5be406e8a983beda1beeb
dwkns <dwkns@me.com>
2022-10-16 13:31:43 +0100

    Updated Config Files

--- scripts/node-project-skeleton.sh
+++ scripts/node-project-skeleton.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env zsh
-. $HOME/.system-config/scripts/set-up-projects.sh $1;
+. $HOME/.system-config/scripts/set-up-projects.sh $1
 
 echo -n "Enable ESLint? ( y/n > default - n) : "
 read -t 3 ESLINT
 ESLINT=${ESLINT:-N}
+
+
+echo -n "Use Typescript? ( y/n > default - n) : "
+read -t 3 TYPESCRIPT
+TYPESCRIPT=${TYPESCRIPT:-N}
 
 success "Project $PROJECTNAME will be created!"
 
@@ -17,78 +22,83 @@ yarn add the-answer --dev
 # yarn add parcel-bundler --dev
 # yarn add parcel-plugin-clean-dist --dev
 
-
 ######## Do some processing of package.json to add build scrips & title.
 
 # change the name property of package.json (need to save temp file first)
 
 # change the name property of package.json (need to save temp file first)
-tmp=$(mktemp) 
+tmp=$(mktemp)
 JQVAR=".name = \"$PROJECTNAME\""
-jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
-
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
 
 # add author  to package.json
-tmp=$(mktemp) 
+tmp=$(mktemp)
 JQVAR=".author = \"$USER\""
-jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
 
-
-tmp=$(mktemp) 
+tmp=$(mktemp)
 JQVAR=".main = \"src/index.js\""
-jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
+
+tmp=$(mktemp)
+JQVAR=".type = \"module\""
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
 
 # add some build scripts to package.json
-tmp=$(mktemp) 
+tmp=$(mktemp)
 JQVAR=".scripts |= .+ { \"start\": \"node src/index.js\" }"
-jq "$JQVAR" package.json > "$tmp" && mv "$tmp" package.json
-
-
-
-
+jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
 
 cat >README.md <<'EOL'
 ##Read Me File
-EOL
 
+To add ESLint Congig:
+
+```bash
+
+```
+EOL
 
 # cat >Gemfile <<'EOL'
 # source 'https://rubygems.org'
 # gem 'rspec'
 # gem 'awesome_print'
 # EOL
-######## Add source folder and files. 
+######## Add source folder and files.
 mkdir 'src'
-mkdir 'bin'
-
-
+# mkdir 'bin'
 
 cat >src/index.js <<'EOL'
-const answer = require( 'the-answer' );
+import answer from 'the-answer'
 console.log( 'the answer is ' + answer );
 EOL
-
 
 cat >.eslintignore <<'EOL'
 **/*
 EOL
 
-if [  "$ESLINT" = "Y" ] || [  "$ESLINT" = "y" ] ; then
- rm -rf .eslintignore
+if [ "$ESLINT" = "Y" ] || [ "$ESLINT" = "y" ]; then
+  rm -rf .eslintignore
+  npx install-peerdeps --dev eslint-config-wesbos
+
+  # Add lint config to package.json
+  tmp=$(mktemp)
+  JQVAR=".eslintConfig |= .+ { \"extends\": [ \"wesbos\" ] }"
+  jq "$JQVAR" package.json >"$tmp" && mv "$tmp" package.json
+
 fi
 
-
-
-
-cat >bin/s <<'EOL'
-#!/usr/bin/env bash
-node ./src/index.js
+cat >.gitignore <<'EOL'
+node_modules
+.env
 EOL
 
+# cat >bin/s <<'EOL'
+# #!/usr/bin/env bash
+# node ./src/index.js
+# EOL
 
-
-chmod +x bin/s
-
+# chmod +x bin/s
 
 ######## Initialize git.
 rm -rf .git #  remove the previous git files.
@@ -96,12 +106,13 @@ git init
 git add .
 git commit -m "Initial commit"
 
-  # rm -rf node_modules.nosync
-  # rm -rf yarn.lock
-  # doing 'removing existing node_modules folder'; rm -rf node_modules
-  # doing 'removing existing node_modules folder'; rm -rf 'node_modules 2'
-  # doing 'creating node_modules.nosync'; mkdir node_modules.nosync
-  # doing 'creating symlink '; ln -s node_modules.nosync/ node_modules
-  doing 'running yarn'; yarn
+# rm -rf node_modules.nosync
+# rm -rf yarn.lock
+# doing 'removing existing node_modules folder'; rm -rf node_modules
+# doing 'removing existing node_modules folder'; rm -rf 'node_modules 2'
+# doing 'creating node_modules.nosync'; mkdir node_modules.nosync
+# doing 'creating symlink '; ln -s node_modules.nosync/ node_modules
+doing 'running yarn'
+yarn
 
 code -r .
